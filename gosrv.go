8c1239a49a1970e5728a9caeb1659632b6fc1cd9
dotSlashLu <dotslash.lu@gmail.com>
2018-07-28 12:26:16 +0800

    gosrv: support cors

--- gosrv.go
+++ gosrv.go
@@ -16,9 +16,20 @@ func parseFlags() {
 	flag.Parse()
 }
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fileServer := http.FileServer(http.Dir("."))
+	enableCors(&w)
+	fileServer.ServeHTTP(w, r)
+}
+
 func main() {
 	parseFlags()
 	fmt.Println("listening on port", port)
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	// http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/", rootHandler)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), nil))
 }
